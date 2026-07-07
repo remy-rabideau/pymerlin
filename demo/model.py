53be0e4e6fb4b2a8fdf76652df6c0ed9d31c6c6d
Remy Rabideau <66102692+remy-rabideau@users.noreply.github.com>
2026-07-07 16:25:54 -0700

    Add more to model demo

--- demo/model.py
+++ demo/model.py
@@ -5,20 +5,44 @@ from pymerlin.model_actions import delay, wait_until, spawn
 @MissionModel
 class Mission:
     def __init__(self, registrar):
-        self.cell1 = registrar.cell("init")
+        self.counter = registrar.cell(0)
+        self.temperature = registrar.cell(20.0)
+        self.status = registrar.cell("idle")
 
-        registrar.resource("/cell1", self.cell1)
-        registrar.topic("/cell1")
-
-@Mission.ActivityType
-def activity1(mission):
-    mission.cell1.emit("foo")
-    result = mission.cell1.get()
-    assert result == "foo", result
-    delay("05:00:00")
-    mission.cell1.emit("bar")
-    spawn(activity2(mission))
+        registrar.resource("/counter", self.counter)
+        registrar.resource("/temperature", self.temperature)
+        registrar.resource("/status", self.status)
+        registrar.topic("/counter")
+        registrar.topic("/temperature")
 
 @Mission.ActivityType
-def activity2(mission):
-    wait_until(lambda: True)
+def increment_counter(mission):
+    current = mission.counter.get()
+    mission.counter.emit(current + 1)
+    delay("00:10:00")
+    
+    if mission.counter.get() < 5:
+        spawn(increment_counter(mission))
+
+@Mission.ActivityType
+def temperature_cycle(mission):
+    mission.status.emit("heating")
+    
+    for temp in [25.0, 30.0, 35.0, 40.0]:
+        delay("00:15:00")
+        mission.temperature.emit(temp)
+    
+    mission.status.emit("cooling")
+    
+    for temp in [35.0, 30.0, 25.0, 20.0]:
+        delay("00:15:00")
+        mission.temperature.emit(temp)
+    
+    mission.status.emit("idle")
+
+@Mission.ActivityType
+def monitor_and_alert(mission):
+    wait_until(lambda: mission.temperature.get() > 30.0)
+    mission.status.emit("alert: high temperature")
+    delay("00:05:00")
+    mission.status.emit("monitoring")
